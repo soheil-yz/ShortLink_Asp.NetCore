5e1198d3f821bf508e603612cb8386e3d9b3eae9
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2024-03-05 13:10:55 +0330

    Create User Repositiry And Interface

---
+++ ShortLink.Infra.Data/Repositories/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using shortLink.Domain.Interface;
+using ShortLink.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortLink.Infra.Data.Repositories
+{
+    internal class UserRepository : IUserRepository
+    {
+        private readonly ShortLinkContext _context;
+        public UserRepository(ShortLinkContext context)
+        {
+            _context = context;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if(_context != null) 
+                await _context.DisposeAsync();
+        }
+    }
+}

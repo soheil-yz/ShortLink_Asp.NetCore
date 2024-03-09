6640017869157f53698d73d678412d1d3d5f2fc1
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2024-03-09 10:19:02 +0330

    Create Some Table And Add-Migration

---
+++ ShortLink.Application/Generetor/Generate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortLink.Application.Generetor
+{
+    public class Generate
+    {
+        public static string Token()
+        {
+            string random = string.Empty;
+
+            Enumerable.Range(48, 57).
+                Where(i => i < 58 || i > 64 && i < 91 || i > 96).
+                OrderBy(i => new Random().Next()).
+                ToList().
+                ForEach(i => random += Convert.ToChar(i));
+
+            return random.Substring(new Random().Next(0, random.Length - 8) - 8);
+        }
+    }
+}
--- ShortLink.Application/obj/Debug/net7.0/ShortLink.Application.AssemblyInfo.cs
+++ ShortLink.Application/obj/Debug/net7.0/ShortLink.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShortLink.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4e6c73666005fad71200dbea518817b7e441fd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3cac8d0bb1fb0da7d6f7b1b1cf278445b04322e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShortLink.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShortLink.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
